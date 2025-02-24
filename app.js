97fff0177117d55ecbb9156eae446a22f14c629b
Van Duong <van.duongngt@gmail.com>
2025-02-24 23:00:35 +0700

    Dinh nghĩa các route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
+const contactsRouter = require("./app/routes/contact.route"); //
 
 app.use(cors());
 app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
-
+app.use("/api/contacts", contactsRouter); //
 module.exports = app;
