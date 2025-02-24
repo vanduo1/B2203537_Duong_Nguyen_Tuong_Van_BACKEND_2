518d57ab8d4696169aa85a63c3282e04a4c01deb
Van Duong <van.duongngt@gmail.com>
2025-02-25 00:35:03 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const contactsRouter = require("./app/routes/contact.route"); //
+const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error"); //** */
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
-app.use("/api/contacts", contactsRouter); //
+
+//** */
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
 module.exports = app;
